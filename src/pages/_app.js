172b7556d3d855b44434f9b06a1dcb2110dc5079
KyannaKrueger <kyannakrueger@gmail.com>
2025-01-12 19:20:04 -0800

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <h1>Nothing to see here. Check back later!</h1>
+  );
 }
