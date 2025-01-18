8bceccd0097c51a17c81944d8fd8bd5c69705856
KyannaKrueger <kyannakrueger@gmail.com>
2025-01-17 17:16:32 -0800

    simple styling completed

--- src/components/molecules/Nav.js
+++ src/components/molecules/Nav.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "../../styles/Nav.module.css";
 
 export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.nav}>
-        <Link href="/">
-          <img 
-            src="Asset 1.png" 
-            alt="Logo" 
-            className={styles.navLogo}
-          />
-        </Link>
-      <ul className={styles.navOptions}>
+      <Link href="/">
+        <img 
+          src="Asset 1.png" 
+          alt="Logo" 
+          className={styles.navLogo}
+        />
+      </Link>
+
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+
+      <ul className={`${styles.navOptions} ${isMenuOpen ? styles.show : ""}`}>
         <li className={styles.navList}><Link href="/" className={styles.navLink}>Home</Link></li>
         <li className={styles.navList}><Link href="/" className={styles.navLink}>About</Link></li>
         <li className={styles.navList}><Link href="/" className={styles.navLink}>Contact</Link></li>
       </ul>
     </nav>
   );
-};
+}
+
 
